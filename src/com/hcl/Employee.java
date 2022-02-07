a2c99a74f738ef533d945abc04d8cb8ff924ec67
AarhySri <aarthysri17@gmail.com>
2022-02-07 09:59:53 +0530

    Krishna Task

--- src/com/hcl/Employee.java
+++ src/com/hcl/Employee.java
@@ -4,14 +4,20 @@ public class Employee {
 		public void Add() {
 			System.out.println("addition");
 		}
-		private void Sub() {
+		public void Sub() {
 			System.out.println("subraction");
 		}
+		private void Mul() {
+			System.out.println("multiplication");
+
+		}
+		
 		public static void main(String[] args) {
 			
 			Employee e = new Employee();
 			e.Add();
 			e.Sub();
+			e.Mul();
 		}
 
 }
