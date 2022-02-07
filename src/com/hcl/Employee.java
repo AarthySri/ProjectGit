501c9d267bcc775cbaa3f9d7330bd99e321f5953
AarhySri <aarthysri17@gmail.com>
2022-02-07 17:29:02 +0530

    Anu task

--- src/com/hcl/Employee.java
+++ src/com/hcl/Employee.java
@@ -4,14 +4,19 @@ public class Employee {
 		public void Add() {
 			System.out.println("addition");
 		}
-		private void Sub() {
+		public void Sub() {
 			System.out.println("subraction");
 		}
+		public void And() {
+			System.out.println("To perform AND operation");
+		}
+		
 		public static void main(String[] args) {
 			
 			Employee e = new Employee();
 			e.Add();
 			e.Sub();
+			e.And();
 		}
 
 }
